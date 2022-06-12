f4b0b5bd97b58dc876b1f86d406a97c63bf4d774
Gim <stgim875@gmail.com>
2022-06-12 22:57:53 +0900

    신규 수업 추가

---
+++ source/repos/ConsoleApp15/ConsoleApp15/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Class1
+    {
+        public int addNumber(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        public void soundFunction()
+        {
+            Console.WriteLine("소리를냅니다.");
+            Console.WriteLine("소리를냅니다.");
+        }
+
+        public void readNumber(int a)
+        {
+            Console.WriteLine("숫자를 출력해주는 함수입니다." +a);
+        }
+    }
+}
---
+++ source/repos/ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //함수
+            int a = 5;
+            int b = 6;
+            //int c = a + b;
+            Class1 class1 = new Class1();
+            int c = class1.addNumber(a, b);
+
+
+            class1.soundFunction();
+            class1.readNumber(10);
+            Console.WriteLine(c);
+        }
+    }
+}
